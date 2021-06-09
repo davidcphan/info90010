2724074dc0e56d053d27f322193d18d832701ce4
David Phan <dphan@ayuda.com.au>
2021-06-09 22:58:53 +1000

    fixed a few issues with API calls

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -4,7 +4,7 @@ window.onload=function(){
         chrome.tabs.create({ url: "https://us05web.zoom.us/s/88665556787?zak=eyJ6bV9za20iOiJ6bV9vMm0iLCJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjbGllbnQiLCJ1aWQiOiI4M2NtcG9ucVItQ295bHRLQUYzbGV3IiwiaXNzIjoid2ViIiwic3R5IjoxMDAsIndjZCI6InVzMDUiLCJjbHQiOjAsInN0ayI6InJsbU5NMGtVeHpPX0l2Y1poQjVES1ljU05YQzI0anVGcWY1R3pCQW02NzguQmdZZ1p6Qk1kRFprUjBKNVptRTRNeXN3VkhaeFdXSktiRUkzTjFWWlNUTjRLMkZBTTJNd05HRTFZakUxTVRNeE9URXlNamRrTkRNM1pHWmhZbUUwWkRKak9XVTVPREJsTURRNFltTXlNV0l6TWpsaFpHWXlNemt6TmpWbE16SXdObUl6TkFBZ01HcHFRblpQTUhadlJubDNRelZJU2pKeE1GTnRWMUEyZGk5b0x6Y3ZSR29BQkhWek1EVUFBQUY1bzRHY09RQVNkUUFBQUEiLCJleHAiOjE2MjE5NTMwNTAsImlhdCI6MTYyMTk0NTg1MCwiYWlkIjoiZlNPaDduMU5Rb1NLQXlWSlV5WEZnQSIsImNpZCI6IiJ9.uNqg-NkqcgcJIarIzkvJv5Nf87tHqh23cmVnwlI2mhI" });
     });
     document.getElementById('space').addEventListener('click',function(){
-        chrome.tabs.create({ url: "http://studymates.davidphan.info/my-groups/" });
+        chrome.tabs.create({ url: "https://studymates.davidphan.info/my-groups/" });
     });
 };
 
@@ -27,7 +27,7 @@ chrome.runtime.onMessage.addListener(
 function getTasks(){
     //Send AJAX request to retrieve task list
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://studymates.davidphan.info/wp-json/api/v1/tasks', true);
+    xhr.open('POST', 'https://studymates.davidphan.info/wp-json/api/v1/tasks', true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.onload = function () {
         var tasks = JSON.parse(this.responseText);
@@ -45,7 +45,7 @@ getTasks();
 function removeTask(x){
     //Send AJAX request to retrieve task list
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://studymates.davidphan.info/wp-json/api/v1/remove-task', true);
+    xhr.open('POST', 'https://studymates.davidphan.info/wp-json/api/v1/remove-task', true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.onload = function () {
         window.location.reload();
@@ -57,7 +57,7 @@ function removeTask(x){
 function completeTask(x){
     //Send AJAX request to retrieve task list
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://studymates.davidphan.info/wp-json/api/v1/complete-task', true);
+    xhr.open('POST', 'https://studymates.davidphan.info/wp-json/api/v1/complete-task', true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.onload = function () {
         window.location.reload();
